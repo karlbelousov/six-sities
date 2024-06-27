15ace95c66d8c9010336779a9184ca2499978755
Karl Belousov <belousovkarl@gmail.com>
2024-06-27 23:56:02 +0600

    компонент лого

--- project/src/pages/main-page/main-page.tsx
+++ project/src/pages/main-page/main-page.tsx
@@ -1,3 +1,4 @@
+import Logo from '../../components/logo/logo';
 import PlaseCard from '../../components/place-card/plase-card';
 
 type MainPageProps = {
@@ -11,15 +12,7 @@ function MainPage({placesCount}: MainPageProps): JSX.Element {
         <div className="container">
           <div className="header__wrapper">
             <div className="header__left">
-              <a className="header__logo-link header__logo-link--active">
-                <img
-                  className="header__logo"
-                  src="img/logo.svg"
-                  alt="6 cities logo"
-                  width={81}
-                  height={41}
-                />
-              </a>
+              <Logo />
             </div>
             <nav className="header__nav">
               <ul className="header__nav-list">
