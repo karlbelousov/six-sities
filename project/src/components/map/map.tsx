a200fc1877c649108107fd91f933599139902168
Karl Belousov <belousovkarl@gmail.com>
2024-07-04 13:31:19 +0600

    создал компонент карты с помощью leaflet

---
+++ project/src/components/map/map.tsx
@@ -0,0 +1,42 @@
+import { useEffect, useRef } from 'react';
+import { City, Location } from '../../types/offer';
+import useMap from '../../hooks/useMap';
+import 'leaflet/dist/leaflet.css';
+import { Marker, Icon } from 'leaflet';
+
+const URL_MARKER_DEFAULT = 'img/pin.svg';
+
+const defaultCustomIcon = new Icon({
+  iconUrl: URL_MARKER_DEFAULT,
+  iconSize: [40, 40],
+  iconAnchor: [20, 40]
+});
+
+type MapProps = {
+  city: City;
+  locations: Location[];
+}
+
+function Map({city, locations}: MapProps) {
+  const mapRef = useRef(null);
+  const map = useMap(mapRef, city);
+
+  useEffect(() => {
+    if (map) {
+      locations.forEach(({latitude: lat, longitude: lng}) => {
+        const marker = new Marker({
+          lat,
+          lng
+        });
+
+        marker.setIcon(defaultCustomIcon).addTo(map);
+      });
+    }
+  }, [map, locations]);
+
+  return (
+    <section className="cities__map map" ref={mapRef}></section>
+  );
+}
+
+export default Map;
