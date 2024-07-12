62953c991976739171e8542536e841435f8caa2e
Karl Belousov <belousovkarl@gmail.com>
2024-07-12 20:12:24 +0600

    подключил новый компонент в offers-list

--- project/src/components/offers-list/offers-list.tsx
+++ project/src/components/offers-list/offers-list.tsx
@@ -1,11 +1,17 @@
 import { useState } from 'react';
 import OfferCard from '../../components/offer-card/offer-card';
 import Map from '../../components/map/map';
-import { useAppSelector } from '../../hooks';
+import { useAppDispatch, useAppSelector } from '../../hooks';
+import SortingList from '../sorting-list/sorting-list';
+import { SortName } from '../../types/types';
+import { setSorting } from '../../store/action';
+import { Comparator } from '../../const';
 
 function OffersList(): JSX.Element {
+  const dispatch = useAppDispatch();
+  const activeSorting = useAppSelector((state) => state.sorting);
   const activeCity = useAppSelector((state) => state.city);
-  const offers = useAppSelector((state) => state.offers.filter((offer) => offer.city.name === state.city.name));
+  const offers = useAppSelector((state) => state.offers.filter((offer) => offer.city.name === state.city.name).sort(Comparator[state.sorting]));
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [activeOffer, setActiveOffer] = useState<number | null>(null);
 
@@ -17,6 +23,10 @@ function OffersList(): JSX.Element {
     setActiveOffer(null);
   };
 
+  const onSortingChange = (name: SortName) => {
+    dispatch(setSorting(name));
+  };
+
   return (
     <>
       <section className="cities__places places">
@@ -24,29 +34,7 @@ function OffersList(): JSX.Element {
         <b className="places__found">
           {offers.length} places to stay in Amsterdam
         </b>
-        <form className="places__sorting" action="#" method="get">
-          <span className="places__sorting-caption">Sort by</span>
-          <span className="places__sorting-type" tabIndex={0}>
-            Popular
-            <svg className="places__sorting-arrow" width={7} height={4}>
-              <use xlinkHref="#icon-arrow-select" />
-            </svg>
-          </span>
-          <ul className="places__options places__options--custom places__options--opened">
-            <li className="places__option places__option--active" tabIndex={0}>
-              Popular
-            </li>
-            <li className="places__option" tabIndex={0}>
-              Price: low to high
-            </li>
-            <li className="places__option" tabIndex={0}>
-              Price: high to low
-            </li>
-            <li className="places__option" tabIndex={0}>
-              Top rated first
-            </li>
-          </ul>
-        </form>
+        <SortingList onChange={onSortingChange} activeSorting={activeSorting} />
         <div className="cities__places-list places__list tabs__content">
           {offers.map((offer) => (
             <OfferCard
