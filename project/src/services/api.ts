6f9d6bcf5cf234f9ee019da5ecc53329efa4932c
Karl Belousov <belousovkarl@gmail.com>
2024-07-19 14:01:02 +0600

    конфигурация axios

---
+++ project/src/services/api.ts
@@ -0,0 +1,13 @@
+import axios, {AxiosInstance} from 'axios';
+
+const BASE_URL = '//10.react.htmlacademy.pro/six-cities';
+const TIMEOT = 5000;
+
+export function createApi(): AxiosInstance {
+  const api = axios.create({
+    baseURL: BASE_URL,
+    timeout: TIMEOT
+  });
+
+  return api;
+}
