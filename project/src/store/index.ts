ae4971bab2ae1f20ad1435246273efa9429fb5f8
Karl Belousov <belousovkarl@gmail.com>
2024-07-19 14:07:46 +0600

    подключил redux-thunk

--- project/src/store/index.ts
+++ project/src/store/index.ts
@@ -1,10 +1,19 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore} from '@reduxjs/toolkit';
 import { reducer } from './reducer';
 import { setOffers } from './action';
 import { offers } from '../mocks/offers';
+import { createApi } from '../services/api';
 
-export const store = configureStore(
-  {reducer}
-);
+export const api = createApi();
+
+export const store = configureStore({
+  reducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      thunk: {
+        extraArgument: api
+      }
+    })
+});
 
 store.dispatch(setOffers(offers));
