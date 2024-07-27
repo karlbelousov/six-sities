baada6b87aa6bde23df6c12356ed3027d8a1e5c1
Karl Belousov <belousovkarl@gmail.com>
2024-07-27 22:18:43 +0600

    рефакторинг ojffers-list: в заголовке plfces to stay in отображаетя выбранный город

--- project/src/components/offers-list/offers-list.tsx
+++ project/src/components/offers-list/offers-list.tsx
@@ -37,7 +37,7 @@ function OffersList(): JSX.Element {
       <section className="cities__places places">
         <h2 className="visually-hidden">Places</h2>
         <b className="places__found">
-          {offers.length} places to stay in Amsterdam
+          {offers.length} places to stay in {activeCity.name}
         </b>
         <SortingList onChange={onSortingChange} activeSorting={activeSorting} />
         <div className="cities__places-list places__list tabs__content">
